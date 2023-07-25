cc4635e21118f6d6090cee35c52570f366cfb671
unknown <sakhuriadachi@gmail.com>
2023-07-25 10:23:52 +0400

    responsive update

--- src/App.jsx
+++ src/App.jsx
@@ -37,10 +37,17 @@ function App() {
   return (
     <>
       <main className="w-full min-h-screen bg-neutral-900 text-white text-3xl flex flex-col items-center justify-center">
-        <header className="w-auto h-40 flex items-center justify-center">
-          <span className="text-[158px] subpixel-antialiased">Mematiane'</span>
-          <span className="w-auto h-20 flex items-end text-neutral-400">
-            designed by <a target="_blank" href="https://www.instagram.com/dachi.s_/" className="mx-3 text-white">@dsakhuria</a>
+        <header className="w-full max-w-2xl h-40 flex flex-col items-center justify-center">
+          <span className="text-5xl sm:text-8xl">Mematiane'</span>
+          <span className="mt-2 text-sm sm:text-base text-neutral-400">
+            designed by{" "}
+            <a
+              target="_blank"
+              href="https://www.instagram.com/dachi.s_/"
+              className="text-white"
+            >
+              @dsakhuria
+            </a>
           </span>
         </header>
         {toggle && (
@@ -54,30 +61,27 @@ function App() {
         )}
         <button
           onClick={openCreationModal}
-          className="border py-4 px-16 
-                    rounded-xl 
-                    ease-out 
-                    mt-16
-                  hover:bg-white hover:text-black"
+          className="border py-4 px-16 rounded-xl mt-8 sm:mt-16
+            hover:bg-white hover:text-black"
         >
-          Add some notes
+          Add notes
         </button>
         {notes.length > 0 && (
-          <div className="w-10/12 h-auto mt-10 py-10 flex flex-col">
+          <div className="w-full max-w-2xl mt-8 sm:mt-10 py-10 flex flex-col">
             <span className="w-full text-center py-6">Notes:</span>
-            <ul
-              className="w-full h-auto bg-[#111] text-neutral-400 rounded-lg flex flex-col justify-center items-center py-6"
-            >
+            <ul className="w-full bg-[#111] text-neutral-400 rounded-lg flex flex-col justify-center items-center py-6">
               {notes.map((note, index) => (
                 <li
                   key={index}
-                  className="list w-11/12 h-auto bg-[#151515] mt-3 mb-3 rounded-lg py-4"
+                  className="list w-full bg-[#151515] mt-3 mb-3 rounded-lg py-4"
                 >
-                  ({index}). {note} 
+                  ({index}). {note}
                   <button
                     onClick={() => {
-                      setNotes((current) => current.filter((_, i) => i !== index));
-                      toast.warning('Note deleted.')
+                      setNotes((current) =>
+                        current.filter((_, i) => i !== index)
+                      );
+                      toast.warning("Note deleted.");
                     }}
                     className="delete hidden px-6 text-white"
                   >
--- src/creationModal.jsx
+++ src/creationModal.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const CreationModal = (props) => {
   return (
-    <div className="w-1/3 rounded-lg h-auto py-10 bg-[#101010] absolute flex flex-col items-center justify-center">
-      <div className="w-10/12 flex justify-end">
-        <button onClick={props.closeCreationModal}>x</button>
+    <div className="w-full md:w-1/3 rounded-lg h-auto py-10 bg-[#101010] absolute flex flex-col items-center justify-center">
+      <div className="w-full md:w-10/12 flex justify-end">
+        <button onClick={props.closeCreationModal} className="text-white">
+          x
+        </button>
       </div>
 
-      <span className="text-white">Create new note</span>
+      <span className="text-white text-lg md:text-xl">Create new note</span>
       <input
         placeholder="Enter text here"
         type="text"
@@ -15,15 +17,14 @@ const CreationModal = (props) => {
         id="newNote"
         value={props.eachNote}
         onChange={props.handleInputChange}
-        className="bg-[#0c0c0c] border rounded-lg 
-                            my-6 
-                            text-xl text-gray-400 
-                            placeholder:text-xl
-                            py-16
-                            px-24
-                            "
+        className="bg-[#0c0c0c] border rounded-lg my-6 text-xl md:text-2xl text-gray-400 placeholder:text-xl py-4 md:py-6 px-6 md:px-12"
       />
-      <button onClick={props.addNotes}>Add</button>
+      <button
+        onClick={props.addNotes}
+        className="px-8 py-2 text-white rounded-lg bg-indigo-600 hover:bg-indigo-700"
+      >
+        Add
+      </button>
     </div>
   );
 };
