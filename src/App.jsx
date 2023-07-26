481ed24e1664eb4c3672013a9fd4bf822d589ed1
unknown <sakhuriadachi@gmail.com>
2023-07-26 17:33:48 +0400

    responsive update v2

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,13 @@ function App() {
               {notes.map((note, index) => (
                 <li
                   key={index}
-                  className="list w-full bg-[#151515] mt-3 mb-3 rounded-lg py-4"
+                  className="
+                  flex text-xl items-center justify-center 
+                  w-full bg-[#151515] mt-3 mb-3 rounded-lg py-4
+                  list
+                  sm:text-3xl
+                  sm:justify-start
+                            "
                 >
                   ({index}). {note}
                   <button
@@ -83,9 +89,14 @@ function App() {
                       );
                       toast.warning("Note deleted.");
                     }}
-                    className="delete hidden px-6 text-white"
+                    className="
+                    flex mx-6 text-white
+                    delete sm:hidden sm:px-6 sm:text-white"
                   >
-                    x
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+
                   </button>
                 </li>
               ))}
--- src/creationModal.jsx
+++ src/creationModal.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 const CreationModal = (props) => {
+  const { closeCreationModal, eachNote, handleInputChange, addNotes } = props;
+
+  // keyUp function for clicking "add" button with enter
+  const handleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      addNotes();
+    }
+  };
+
   return (
     <div className="w-full md:w-1/3 rounded-lg h-auto py-10 bg-[#101010] absolute flex flex-col items-center justify-center">
       <div className="w-full md:w-10/12 flex justify-end">
-        <button onClick={props.closeCreationModal} className="text-white">
+        <button onClick={closeCreationModal} className="text-white">
           x
         </button>
       </div>
@@ -15,12 +24,13 @@ const CreationModal = (props) => {
         type="text"
         name="newNote"
         id="newNote"
-        value={props.eachNote}
-        onChange={props.handleInputChange}
+        value={eachNote}
+        onChange={handleInputChange}
+        onKeyUp={handleKeyUp} 
         className="bg-[#0c0c0c] border rounded-lg my-6 text-xl md:text-2xl text-gray-400 placeholder:text-xl py-4 md:py-6 px-6 md:px-12"
       />
       <button
-        onClick={props.addNotes}
+        onClick={addNotes}
         className="px-8 py-2 text-white rounded-lg bg-indigo-600 hover:bg-indigo-700"
       >
         Add
